b22df17017ec595e0c71bc075bce028f3765901b
Remo <43846137+RemGal@users.noreply.github.com>
2020-07-12 02:20:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var przeszlosc = document.getElementById("przeszlosc");
 var rola = document.getElementById("rola");
 alert(pochodzenie.value + przeszlosc.value + rola.value);
 
-var pdf = new jsPDF();
+var pdf = new jsPDF('p', 'pt', 'a4');
 pdf.addImage(getImageFromUrl("images/" + pochodzenie.value + ".png"), "PNG", 0, 0, 1654, 2339, "karta", 180, 180);
 
 
