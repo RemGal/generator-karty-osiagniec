4aa857e088c61c324afa3801365f8c5364f00d78
Remo <43846137+RemGal@users.noreply.github.com>
2020-07-12 02:24:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ var rola = document.getElementById("rola");
 alert(pochodzenie.value + przeszlosc.value + rola.value);
 
 var pdf = new jsPDF('p', 'pt', 'a4');
-var imm = getImageFromUrl("images/" + pochodzenie.value + ".png");
-pdf.addImage(img, "PNG", 0, 0, 100, 100, "karta", 180, 180);
+
+pdf.addImage(getImageFromUrl("images/" + pochodzenie.value + ".png"), "PNG", 0, 0, 100, 100, "karta", 180, 180);
 
 
 pdf.save('karta_osiagniec.pdf');
