be72558b3d9d339a5c6f0f270771e18471e38fef
Remo <43846137+RemGal@users.noreply.github.com>
2020-07-12 02:03:15 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function getImageFromUrl(url)
+{
+var img = new Image();
+img.src = url;
+return img;
+}
+
+
+function PobierzKarte()
+{
+var pochodzenie = document.getElementById("pochodzenie");
+var przeszlosc = document.getElementById("przeszlosc");
+var rola = document.getElementById("rola");
+alert(pochodzenie.value + przeszlosc.value + rola.value);
+
+var pdf = new jsPDF();
+pdf.addImage(getImageFromUrl("images/" + pochodzenie.value + ".png"), "PNG", 0, 0, 1654, 2339, 0, 0, 0)
+
+
+pdf.save('karta_osiagniec.pdf');
+}
