3be25ebfa941dacdcf51946b35400feca94380f8
Remo <43846137+RemGal@users.noreply.github.com>
2020-07-12 02:16:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var rola = document.getElementById("rola");
 alert(pochodzenie.value + przeszlosc.value + rola.value);
 
 var pdf = new jsPDF();
-pdf.addImage(getImageFromUrl("images/" + pochodzenie.value + ".png"), "PNG");
+pdf.addImage(getImageFromUrl("images/" + pochodzenie.value + ".png"), "PNG", 0, 0, 100, 100, 180, 180);
 
 
 pdf.save('karta_osiagniec.pdf');
