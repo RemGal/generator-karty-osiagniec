b17ae8c2dbc99e556f0ea3aeeb2a286820ca7546
Remo <43846137+RemGal@users.noreply.github.com>
2020-07-12 02:42:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ alert(pochodzenie.value + przeszlosc.value + rola.value);
 
 var pdf = new jsPDF();
 
-pdf.addImage(getImageFromUrl("images/" + pochodzenie.value + ".png"), "PNG");
+pdf.addImage(getImageFromUrl("images/" + pochodzenie.value + ".png"), "PNG", 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight(), "karta", 180);
 
 
 pdf.save('karta_osiagniec.pdf');
