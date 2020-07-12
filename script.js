f069339394354dba08327d9b9fc14f7082aba5f4
Remo <43846137+RemGal@users.noreply.github.com>
2020-07-12 02:12:57 +0200

    testing pdf.addImage()

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var rola = document.getElementById("rola");
 alert(pochodzenie.value + przeszlosc.value + rola.value);
 
 var pdf = new jsPDF();
-pdf.addImage(getImageFromUrl("images/" + pochodzenie.value + ".png"), "PNG", 0, 0, 1654, 2339, "karta", 1, 90);
+pdf.addImage(getImageFromUrl("images/" + pochodzenie.value + ".png"), "PNG", 0, 0, 1654, 2339, "karta", 100, 0);
 
 
 pdf.save('karta_osiagniec.pdf');
