f770b49503346bf90d25a145ec74770601e79cca
Remo <43846137+RemGal@users.noreply.github.com>
2020-07-12 03:58:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-function getImage(dir)
+function getImageFromUrl(url)
 {
-  var img = new Image();
-  img.src = dir;
-  return img;
+var img = new Image();
+img.src = url;
+return img;
 }
+
 
 function PobierzKarte()
 {
-  var pochodzenie = document.getElementById("pochodzenie");
-  var przeszlosc = document.getElementById("przeszlosc");
-  var rola = document.getElementById("rola");
-  var pdf = new jsPDF();
-  pdf.addImage(getImage("images/" + pochodzenie.value + ".png"), "PNG", 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight());
-  pdf.addImage(getImage("images/" + przeszlosc.value + ".png"), "PNG", 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight());
-  pdf.addImage(getImage("images/" + rola.value + ".png"), "PNG", 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight());
-  pdf.save('karta_osiagniec.pdf');
+var pochodzenie = document.getElementById("pochodzenie");
+var przeszlosc = document.getElementById("przeszlosc");
+var rola = document.getElementById("rola");
+alert(pochodzenie.value + przeszlosc.value + rola.value);
+
+var pdf = new jsPDF();
+pdf.addImage(getImageFromUrl("images/" + pochodzenie.value + ".png"), "PNG", 0, 0, 1, 1, 0, 0, 0)
+
+
+pdf.save('karta_osiagniec.pdf');
 }
