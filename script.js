d8d243fe7736bfa99176d67a08b69a4cd59cd9fb
Remo <43846137+RemGal@users.noreply.github.com>
2020-09-05 21:45:56 +0200

    code refactored

--- script.js
+++ script.js
@@ -12,8 +12,12 @@ function PobierzKarte()
   var rola = document.getElementById("rola");
   const { jsPDF } = window.jspdf;
   var pdf = new jsPDF();
-  pdf.addImage(getImage("images/" + pochodzenie.value + ".png"), "PNG", 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight());
-  pdf.addImage(getImage("images/" + przeszlosc.value + ".png"), "PNG", 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight());
-  pdf.addImage(getImage("images/" + rola.value + ".png"), "PNG", 0, 0, pdf.internal.pageSize.getWidth(), pdf.internal.pageSize.getHeight());
+  var pageSize = {
+    height: pdf.internal.pageSize.getHeight(),
+    width: pdf.internal.pageSize.getWidth()
+  }
+  pdf.addImage(getImage("images/" + pochodzenie.value + ".png"), "PNG", 0, 0, pageSize.width, pageSize.height);
+  pdf.addImage(getImage("images/" + przeszlosc.value + ".png"), "PNG", 0, 0, pageSize.width, pageSize.height);
+  pdf.addImage(getImage("images/" + rola.value + ".png"), "PNG", 0, 0, pageSize.width, pageSize.height);
   pdf.save('karta_osiagniec.pdf');
 }
